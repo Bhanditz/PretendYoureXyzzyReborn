ac78dfb28459be27bb7583d09a45e24b51410372
Gianlu <altomanigianluca@gmail.com>
2017-08-02 16:10:22 +0200

    Delete useful files

--- src/com/gianlu/pyxreborn/Config.java
+++
@@ -1,8 +0,0 @@
-package com.gianlu.pyxreborn;
-
-import com.beust.jcommander.Parameter;
-
-public class Config {
-    @Parameter(names = {"--server-port"}, description = "Server will listen for request on this port", required = true)
-    public int serverPort;
-}

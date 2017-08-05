090af1772b0d1e3e932c116732291b5ccbde2e73
Gianlu <altomanigianluca@gmail.com>
2017-08-05 15:09:43 +0200

    Client interface improved

--- client/src/com/gianlu/pyxreborn/client/Startup.java
+++ client/src/com/gianlu/pyxreborn/client/Startup.java
@@ -13,7 +13,9 @@ import com.gianlu.consoleui.Input.InputValidator;
 import com.gianlu.consoleui.InvalidInputException;
 import com.gianlu.pyxreborn.Fields;
 import com.gianlu.pyxreborn.Operations;
-import org.fusesource.jansi.Ansi;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import org.fusesource.jansi.AnsiConsole;
 import org.jetbrains.annotations.NotNull;
 
@@ -26,6 +28,7 @@ public class Startup {
     private static final String LOGGING = "logging";
     private static final String NEXT_ACTION = "nextAction";
     private final ConsolePrompt prompt = new ConsolePrompt();
+    private Client client;
 
     public static void main(String[] args) throws InterruptedException, IOException {
         AnsiConsole.systemInstall();
@@ -61,73 +64,116 @@ public class Startup {
                         .build());
 
         Logger.setEnabled(((ConfirmationAnswer) result.get(0)).isConfirmed());
-        Client client = new Client(URI.create(((InputAnswer) result.get(1)).getAnswer()),
+
+        startup.client = new Client(URI.create(((InputAnswer) result.get(1)).getAnswer()),
                 ((InputAnswer) result.get(2)).getAnswer());
 
-        if (client.connectBlocking()) {
+        if (startup.client.connectBlocking()) {
             startup.mainMenu();
         } else {
             Logger.severe(new Exception("Failed connecting!"));
         }
     }
 
-    private void clear() {
-        Ansi.ansi().eraseScreen();
+    private void createGame() {
+        throw new UnsupportedOperationException("Not implemented yet!"); // TODO
     }
 
-    private void mainMenu() throws IOException {
-        clear();
-
-        ChoiceAnswer result = prompt.prompt(new ChoicePrompt.Builder()
-                .name(NEXT_ACTION)
-                .text("What you wanna do next?")
-                .newItem().text("List games").key('g').name(Operations.GET_GAMES_LIST.toString()).add()
-                .newItem().text("List users").key('u').name(Operations.GET_USERS_LIST.toString()).add()
-                .newItem().text("Create game").key('c').name(Operations.CREATE_GAME.toString()).add()
-                .build());
+    private void mainMenu() {
+        ChoiceAnswer result;
+        try {
+            result = prompt.prompt(new ChoicePrompt.Builder()
+                    .name(NEXT_ACTION)
+                    .text("What you wanna do next?")
+                    .newItem().text("List games").key('g').name(Operations.GET_GAMES_LIST.toString()).add()
+                    .newItem().text("List users").key('u').name(Operations.GET_USERS_LIST.toString()).add()
+                    .newItem().text("Create game").key('c').name(Operations.CREATE_GAME.toString()).add()
+                    .build());
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
 
         Operations next = Operations.parse(result.getName());
-        System.out.println("NEXT: " + next);
+        if (next == null) throw new IllegalArgumentException("next can't be null!");
 
-        /*
-        client.sendMessage(client.createRequest(Operations.GET_GAMES_LIST), new PyxClientAdapter.IMessage() {
-            @Override
-            public void onMessage(JsonObject resp) {
-                JsonArray games = resp.getAsJsonArray(Fields.GAMES_LIST.toString());
-                if (games.size() == 0) {
-                    System.out.println("There are no games!");
-                    return;
-                }
+        switch (next) {
+            case GET_GAMES_LIST:
+                client.sendMessage(client.createRequest(Operations.GET_GAMES_LIST), new PyxClientAdapter.IMessage() {
+                    @Override
+                    public void onMessage(JsonObject resp) {
+                        JsonArray games = resp.getAsJsonArray(Fields.GAMES_LIST.toString());
+                        if (games.size() == 0) {
+                            try {
+                                ConfirmationAnswer answer = prompt.prompt(new ConfirmationPrompt.Builder()
+                                        .text("There are no games! Do you want to create one?")
+                                        .defaultValue(Value.YES)
+                                        .name("createGame")
+                                        .build());
 
-                PromptBuilder builder = prompt.getPromptBuilder();
-                ListPromptBuilder gamesPrompt = builder.createListPrompt()
-                        .name(Fields.GID.toString())
-                        .message("Select a game to join:");
+                                if (answer.isConfirmed()) createGame();
+                                else mainMenu();
+                            } catch (IOException ex) {
+                                ex.printStackTrace();
+                            }
 
-                for (JsonElement game : games)
-                    gamesPrompt.newItem()
-                            .text(game.getAsJsonObject()
-                                    .getAsJsonObject(Fields.HOST.toString())
-                                    .get(Fields.NICKNAME.toString()).getAsString())
-                            .name(game.getAsJsonObject()
-                                    .get(Fields.GID.toString()).getAsString())
-                            .add();
+                            return;
+                        }
 
-                gamesPrompt.addPrompt();
+                        ChoicePrompt.Builder builder = new ChoicePrompt.Builder();
+                        builder.name(Fields.GID.toString())
+                                .text("Select a game to join:");
 
-                try {
-                    Map<String, ? extends PromtResultItemIF> result = prompt.prompt(builder.build());
-                    System.out.println("GAME SELECTED: " + ((ListResult) result.get(Fields.GID.toString())).getSelectedId());
-                } catch (IOException ex) {
-                    onException(ex);
-                }
-            }
+                        // TODO: I must use ListChoicePrompt but it's not finished, this doesn't work.
+                        for (JsonElement game : games)
+                            builder.newItem()
+                                    .text(game.getAsJsonObject()
+                                            .getAsJsonObject(Fields.HOST.toString())
+                                            .get(Fields.NICKNAME.toString()).getAsString())
+                                    .name(game.getAsJsonObject()
+                                            .get(Fields.GID.toString()).getAsString())
+                                    .add();
 
-            @Override
-            public void onException(Exception ex) {
-                ex.printStackTrace();
-            }
-        });
-        */
+                        try {
+                            ChoiceAnswer answer = prompt.prompt(builder.build());
+                            System.out.println(answer);
+                        } catch (IOException ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onException(Exception ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                break;
+            case GET_USERS_LIST:
+                client.sendMessage(client.createRequest(Operations.GET_USERS_LIST), new PyxClientAdapter.IMessage() {
+                    @Override
+                    public void onMessage(JsonObject resp) {
+                        JsonArray users = resp.getAsJsonArray(Fields.USERS_LIST.toString());
+
+                        StringBuilder builder = new StringBuilder();
+                        boolean first = true;
+                        for (JsonElement element : users) {
+                            if (!first) builder.append(", ");
+                            first = false;
+                            builder.append(element.getAsJsonObject().get(Fields.NICKNAME.toString()).getAsString());
+                        }
+
+                        System.out.println("Online users: " + builder.toString());
+                        mainMenu();
+                    }
+
+                    @Override
+                    public void onException(Exception ex) {
+                        ex.printStackTrace();
+                    }
+                });
+                break;
+            case CREATE_GAME:
+                createGame();
+                break;
+        }
     }
 }
--- lib/src/com/gianlu/pyxreborn/Utils.java
+++ lib/src/com/gianlu/pyxreborn/Utils.java
@@ -2,17 +2,9 @@ package com.gianlu.pyxreborn;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Utils {
     public static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    public static <K, V> Map<K, V> singletonMap(K key, V value) {
-        Map<K, V> map = new HashMap<>();
-        map.put(key, value);
-        return map;
-    }
 
     public static String printStackTrace(Throwable ex) {
         StringWriter string = new StringWriter();
